45f4b08e58c9e1d6f5ac15e0f0c1c59ba88276eb
Mrityunjay Kumar <mjaycse@gmail.com>
2019-08-18 22:24:57 -0400

    test fibonacci solution on EPI jugde

--- epi_judge_cpp/fibonacci.cc
+++ epi_judge_cpp/fibonacci.cc
@@ -1,7 +1,27 @@
 #include "test_framework/generic_test.h"
+#include <vector>
 int Fibonacci(int n) {
   // TODO - you fill in here.
-  return -1;
+  // base cases
+  // when n is negative
+  // when n is 0
+  if(n<=0)
+    return 0;
+  // when n is 1
+  if(n==1)
+	return 1;
+  // create a vector of size n
+  // start filling the vector in bottom up manner
+  std::vector<int> nums;
+  nums.reserve (n+1);
+  nums[0]=0;
+  nums[1]=1;
+  
+  for (int i = 2; i <= n ; ++i) {
+	// lookup prev two for this result
+	nums[i] = nums[i-1]+nums[i-2];
+  }
+  return nums[n];
 }
 
 int main(int argc, char* argv[]) {
